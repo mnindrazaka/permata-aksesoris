a47c13c0a03b6e643b5bc6b9750cfd952d1805e8
M. Nindra Zaka <nindra.zaka@ruangguru.com>
2022-10-26 14:08:24 +0700

    feat: implement table join with product and category

--- apps/api/modules/products/schema.go
+++ apps/api/modules/products/schema.go
@@ -1,9 +1,13 @@
 package products
 
+import "permata-aksesoris/apps/api/modules/categories"
+
 type Product struct {
-	Serial      string `json:"serial" gorm:"primaryKey"`
-	Title       string `json:"title"`
-	Slug        string `json:"slug"`
-	Thumbnail   string `json:"thumbnail"`
-	Description string `json:"description"`
+	Serial         string              `json:"serial" gorm:"primaryKey"`
+	Title          string              `json:"title"`
+	Slug           string              `json:"slug"`
+	Thumbnail      string              `json:"thumbnail"`
+	Description    string              `json:"description"`
+	CategorySerial string              `json:"-"`
+	Category       categories.Category `json:"category" gorm:"foreignKey:CategorySerial"`
 }

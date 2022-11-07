8725a46d4920c37d2b1d636d5a8d3a0700ae7a3d
M. Nindra Zaka <nindra.zaka@ruangguru.com>
2022-11-07 21:37:15 +0700

    feat: implement auth middleware on some routes

--- apps/api/modules/categories/router.go
+++ apps/api/modules/categories/router.go
@@ -9,6 +9,6 @@ import (
 func NewRouter(handler Handler, router *mux.Router) {
 	router.HandleFunc("/categories", handler.getCategories).Methods("GET")
 	router.HandleFunc("/categories", middlewares.NewAuthenticateMiddleware(handler.createCategory)).Methods("POST")
-	router.HandleFunc("/categories/{serial}", handler.updateCategory).Methods("PUT")
-	router.HandleFunc("/categories/{serial}", handler.deleteCategory).Methods("DELETE")
+	router.HandleFunc("/categories/{serial}", middlewares.NewAuthenticateMiddleware(handler.updateCategory)).Methods("PUT")
+	router.HandleFunc("/categories/{serial}", middlewares.NewAuthenticateMiddleware(handler.deleteCategory)).Methods("DELETE")
 }
--- apps/api/modules/products/router.go
+++ apps/api/modules/products/router.go
@@ -1,14 +1,18 @@
 package products
 
-import "github.com/gorilla/mux"
+import (
+	"permata-aksesoris/apps/api/middlewares"
+
+	"github.com/gorilla/mux"
+)
 
 func NewRouter(handler Handler, router *mux.Router) {
 	router.HandleFunc("/products", handler.getProducts).Methods("GET")
 	router.HandleFunc("/products/{serial}", handler.getProductDetail).Methods("GET")
-	router.HandleFunc("/products", handler.createProduct).Methods("POST")
-	router.HandleFunc("/products/{serial}", handler.updateProduct).Methods("PUT")
-	router.HandleFunc("/products/{serial}", handler.deleteProduct).Methods("DELETE")
+	router.HandleFunc("/products", middlewares.NewAuthenticateMiddleware(handler.createProduct)).Methods("POST")
+	router.HandleFunc("/products/{serial}", middlewares.NewAuthenticateMiddleware(handler.updateProduct)).Methods("PUT")
+	router.HandleFunc("/products/{serial}", middlewares.NewAuthenticateMiddleware(handler.deleteProduct)).Methods("DELETE")
 
-	router.HandleFunc("/products/{productSerial}/images", handler.createProductImage).Methods("POST")
-	router.HandleFunc("/products/{productSerial}/images/{imageSerial}", handler.deleteProductImage).Methods("DELETE")
+	router.HandleFunc("/products/{productSerial}/images", middlewares.NewAuthenticateMiddleware(handler.createProductImage)).Methods("POST")
+	router.HandleFunc("/products/{productSerial}/images/{imageSerial}", middlewares.NewAuthenticateMiddleware(handler.deleteProductImage)).Methods("DELETE")
 }

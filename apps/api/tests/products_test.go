27f8e7e95d9b9ca007c916956091d1848a52ce8c
M. Nindra Zaka <nindra.zaka@ruangguru.com>
2022-11-18 04:37:52 +0700

    feat: implement test to create product image

--- apps/api/tests/products_test.go
+++ apps/api/tests/products_test.go
@@ -401,3 +401,91 @@ func TestDeleteProduct(t *testing.T) {
 		log.Fatal(err)
 	}
 }
+
+func TestCreateProductImage(t *testing.T) {
+	con, err := databases.NewTestDBConnection()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := databases.Migrate(con); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := databases.Unseed(con); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := databases.Seed(con); err != nil {
+		log.Fatal(err)
+	}
+
+	router := mux.NewRouter().StrictSlash(true)
+	productRepository := products.NewRepository(con)
+	productUsecase := products.NewUsecase(productRepository)
+	productHandler := products.NewHandler(productUsecase)
+	products.NewRouter(productHandler, router)
+
+	userRepository := users.NewRepository(con)
+	userUsecase := users.NewUsecase(userRepository)
+	userHandler := users.NewHandler(userUsecase)
+	users.NewRouter(userHandler, router)
+
+	reqBody := map[string]interface{}{
+		"email":    "admin@gmail.com",
+		"password": "admin",
+	}
+	body, err := json.Marshal(reqBody)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := httptest.NewRequest("POST", "/users/login", bytes.NewReader(body))
+	w := httptest.NewRecorder()
+
+	router.ServeHTTP(w, r)
+
+	reqBody = map[string]interface{}{
+		"url": "https://picsum.photos/200/300",
+	}
+	body, err = json.Marshal(reqBody)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data, err := ioutil.ReadAll(w.Result().Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var response ResponseJWT
+	if err := json.Unmarshal(data, &response); err != nil {
+		log.Fatal(err)
+	}
+
+	r = httptest.NewRequest("POST", "/products/"+databases.ProductsData[0].Serial+"/images", bytes.NewReader(body))
+	r.Header.Set("Authorization", "Bearer "+response.Data["token"])
+	w = httptest.NewRecorder()
+
+	router.ServeHTTP(w, r)
+
+	data, err = ioutil.ReadAll(w.Result().Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var responseCategory utils.Response
+	if err := json.Unmarshal(data, &responseCategory); err != nil {
+		log.Fatal(err)
+	}
+
+	assert.Equal(t, fmt.Sprint(utils.Response{
+		Data:    nil,
+		Status:  http.StatusText(http.StatusOK),
+		Message: http.StatusText(http.StatusOK),
+	}), fmt.Sprint(responseCategory))
+
+	if err := databases.Unseed(con); err != nil {
+		log.Fatal(err)
+	}
+}

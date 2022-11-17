b3f78d623bbc9f6b8493cd4569efa138e4b2b1d9
M. Nindra Zaka <nindra.zaka@ruangguru.com>
2022-11-18 03:50:40 +0700

    feat: implement create category

--- apps/api/tests/categories_test.go
+++ apps/api/tests/categories_test.go
@@ -1,13 +1,16 @@
 package tests
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"net/http/httptest"
 	"permata-aksesoris/apps/api/databases"
 	"permata-aksesoris/apps/api/modules/categories"
+	"permata-aksesoris/apps/api/modules/users"
 	"permata-aksesoris/apps/api/utils"
 	"testing"
 
@@ -64,6 +67,102 @@ func TestGetCategories(t *testing.T) {
 	}
 
 	assert.Equal(t, fmt.Sprint(expectedData), fmt.Sprint(response.Data))
+
+	if err := databases.Unseed(con); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type ResponseJWT struct {
+	Data    map[string]string `json:"data"`
+	Status  string            `json:"status"`
+	Message string            `json:"message"`
+}
+
+func TestCreateCategory(t *testing.T) {
+	con, err := databases.NewTestDBConnection()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := databases.Migrate(con); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := databases.Unseed(con); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := databases.Seed(con); err != nil {
+		log.Fatal(err)
+	}
+
+	router := mux.NewRouter().StrictSlash(true)
+	categoryRepository := categories.NewRepository(con)
+	categoryUsecase := categories.NewUsecase(categoryRepository)
+	categoryHandler := categories.NewHandler(categoryUsecase)
+	categories.NewRouter(categoryHandler, router)
+
+	userRepository := users.NewRepository(con)
+	userUsecase := users.NewUsecase(userRepository)
+	userHandler := users.NewHandler(userUsecase)
+	users.NewRouter(userHandler, router)
+
+	reqBody := map[string]interface{}{
+		"email":    "admin@gmail.com",
+		"password": "admin",
+	}
+	body, err := json.Marshal(reqBody)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := httptest.NewRequest("POST", "/users/login", bytes.NewReader(body))
+	w := httptest.NewRecorder()
+
+	router.ServeHTTP(w, r)
+
+	reqBody = map[string]interface{}{
+		"title": "New Category",
+		"slug":  "new-category",
+		"icon":  "",
+	}
+	body, err = json.Marshal(reqBody)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data, err := ioutil.ReadAll(w.Result().Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var response ResponseJWT
+	if err := json.Unmarshal(data, &response); err != nil {
+		log.Fatal(err)
+	}
+
+	r = httptest.NewRequest("POST", "/categories", bytes.NewReader(body))
+	r.Header.Set("Authorization", "Bearer "+response.Data["token"])
+	w = httptest.NewRecorder()
+
+	router.ServeHTTP(w, r)
+
+	data, err = ioutil.ReadAll(w.Result().Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var responseCategory utils.Response
+	if err := json.Unmarshal(data, &responseCategory); err != nil {
+		log.Fatal(err)
+	}
+
+	assert.Equal(t, fmt.Sprint(utils.Response{
+		Data:    nil,
+		Status:  http.StatusText(http.StatusOK),
+		Message: http.StatusText(http.StatusOK),
+	}), fmt.Sprint(responseCategory))
 
 	if err := databases.Unseed(con); err != nil {
 		log.Fatal(err)
